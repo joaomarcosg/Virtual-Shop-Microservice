ab9dd8cd5a4e54b32c6ac20e7f12570c7f304c3c
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-11-04 17:44:02 -0300

    FEAT: adds Get and GetCategoriesProducts endpoints

--- VShop.ProductApi/Controllers/CategoriesController.cs
+++ VShop.ProductApi/Controllers/CategoriesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using VShop.ProductApi.DTOs;
 using VShop.ProductApi.Services;
 
 namespace VShop.ProductApi.Controllers
@@ -12,6 +13,25 @@ namespace VShop.ProductApi.Controllers
     public class CategoriesController : ControllerBase
     {
         private readonly ICategoryService _categoryService;
-        
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categoriesDTO = await _categoryService.GetCategories();
+            if (categoriesDTO is null)
+                return NotFound("Categories not found");
+            return Ok(categoriesDTO);
+        }
+        [HttpGet("products")]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetCategoriesProducts()
+        {
+            var categoriesDTO = await _categoryService.GetCategoriesProducts();
+            if (categoriesDTO is null)
+                return NotFound("Categories not found");
+            return Ok(categoriesDTO);
+        }
     }
 }
