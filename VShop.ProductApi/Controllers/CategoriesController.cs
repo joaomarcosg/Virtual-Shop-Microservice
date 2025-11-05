8da4c883ecc40c82abad0bdae3831394104a5844
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-11-05 16:29:33 -0300

    FEAT: adds GetCategory, Post, Put and Delete enpoints

--- VShop.ProductApi/Controllers/CategoriesController.cs
+++ VShop.ProductApi/Controllers/CategoriesController.cs
@@ -25,6 +25,7 @@ namespace VShop.ProductApi.Controllers
                 return NotFound("Categories not found");
             return Ok(categoriesDTO);
         }
+
         [HttpGet("products")]
         public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetCategoriesProducts()
         {
@@ -33,5 +34,52 @@ namespace VShop.ProductApi.Controllers
                 return NotFound("Categories not found");
             return Ok(categoriesDTO);
         }
+
+        [HttpGet("{id:int}", Name = "GetCategory")]
+        public async Task<ActionResult<CategoryDTO>> Get(int id)
+        {
+            var categoryDTO = await _categoryService.GetCategoryById(id);
+            if (categoryDTO is null)
+                return NotFound("Category not found");
+            return Ok(categoryDTO);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CategoryDTO categoryDTO)
+        {
+            if (categoryDTO is null)
+                return BadRequest("Invalid data");
+
+            await _categoryService.AddCategory(categoryDTO);
+
+            return new CreatedAtRouteResult("GetCategory", new { id = categoryDTO.CategoryId });
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, [FromBody] CategoryDTO categoryDTO)
+        {
+            if (id != categoryDTO.CategoryId)
+                return BadRequest("Invalid id");
+
+            if (categoryDTO is null)
+                return BadRequest("Invalid data");
+
+            await _categoryService.UpdateCategory(categoryDTO);
+
+            return Ok(categoryDTO);
+        }
+
+        [HttpDelete("{id: int}")]
+        public async Task<ActionResult<CategoryDTO>> Delete (int id)
+        {
+            var categoryDTO = await _categoryService.GetCategoryById(id);
+            if (categoryDTO is null)
+                return NotFound("Category not found");
+
+            await _categoryService.RemoveCategory(id);
+
+            return Ok(categoryDTO);
+        }
+
     }
 }
