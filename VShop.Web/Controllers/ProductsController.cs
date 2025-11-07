05bfd8c05b410514c6f4f1656bb212d99a4e3053
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-11-07 16:48:27 -0300

    FEAT: adds ProductsController

---
+++ VShop.Web/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VShop.Web.Models;
+using VShop.Web.Services.Contracts;
+
+namespace VShop.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductViewModel>>> Index()
+        {
+            var result = await _productService.GetAllProducts();
+
+            if (result is null)
+                return View("Error");
+
+            return View(result);
+        }
+
+    }
+}
