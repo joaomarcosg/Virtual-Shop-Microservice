2e816a72f6459b4b01020aee1d4785fca289a239
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-11-12 15:59:58 -0300

    FEAT: adds CreateProduct HTTP POST method

--- VShop.Web/Controllers/ProductsController.cs
+++ VShop.Web/Controllers/ProductsController.cs
@@ -15,9 +15,11 @@ namespace VShop.Web.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
-        public ProductsController(IProductService productService)
+        private readonly ICategoryService _categoryService;
+        public ProductsController(IProductService productService, ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         [HttpGet]
@@ -38,6 +40,25 @@ namespace VShop.Web.Controllers
                 _categoryService.GetAllCategories(), "CategoryId", "Name");
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(ProductViewModel productVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _productService.CreateProduct(productVM);
+
+                if (result != null)
+                    return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                ViewBag.CategoryId = new SelectList(await
+                                     _categoryService.GetAllCategories(), "CategoryId", "Name");
+            }
+            return View(productVM);
+        }
+
 
     }
 }
