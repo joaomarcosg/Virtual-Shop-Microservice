69b765d5dfe5354e67e4cd6201785957f2c873ef
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-11-12 15:38:34 -0300

    FEAT: adds CreateProdut controller method

--- VShop.Web/Controllers/ProductsController.cs
+++ VShop.Web/Controllers/ProductsController.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using VShop.Web.Models;
 using VShop.Web.Services.Contracts;
@@ -13,7 +15,6 @@ namespace VShop.Web.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
-
         public ProductsController(IProductService productService)
         {
             _productService = productService;
@@ -28,6 +29,14 @@ namespace VShop.Web.Controllers
                 return View("Error");
 
             return View(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateProduct()
+        {
+            ViewBag.CategoryId = new SelectList(await
+                _categoryService.GetAllCategories(), "CategoryId", "Name");
+            return View();
         }
 
     }
